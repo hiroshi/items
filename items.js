e743d919c188ec69cc4dfe65250021d230754cce
Hiroshi Saito <hiroshi3110@gmail.com>
2013-11-16 10:32:59 +0900

    Replace routers models implementations with DropboxdataStoreAdapter

--- items.js
+++ items.js
@@ -12,20 +12,55 @@
     //LOG_TRANSITIONS: true, 
     //LOG_TRANSITIONS_INTERNAL: true
   });
-  // promise for Dropbox datastore
-  App.datastore = new Ember.RSVP.Promise(function (resolve, reject) {
-    if (client.isAuthenticated()) {
-      var datastoreManager = client.getDatastoreManager();
-      datastoreManager.openDefaultDatastore(function (error, datastore) {
-        if (error) {
-          alert('Error opening default datastore: ' + error);
-          reject(errot);
-        } else {
-          resolve(datastore);
-        }
+  // Dropbox DataStore API Ember.js Data Adapter
+  App.DropboxDataStoreAdapter = DS.Adapter.extend({
+    // promise for Dropbox datastore
+    _datastore: new Ember.RSVP.Promise(function (resolve, reject) {
+      if (client.isAuthenticated()) {
+        var datastoreManager = client.getDatastoreManager();
+        datastoreManager.openDefaultDatastore(function (error, datastore) {
+          if (error) {
+            alert('Error opening default datastore: ' + error);
+            reject(errot);
+          } else {
+            resolve(datastore);
+          }
+        });
+      }
+    }),
+    find: function(store, type, id) {
+      return this._datastore.then(function(datastore) {
+        return new Ember.RSVP.Promise(function (resolve, reject) {
+          var table = datastore.getTable(type.tableName);
+          var record = table.get(id);
+          var value = $.extend(record.getFields(), {"id": record.getId()});
+          resolve(value);
+        });
+      });
+    },
+    findAll: function(store, type, since) {
+      return this._datastore.then(function(datastore) {
+        return new Ember.RSVP.Promise(function (resolve, reject) {
+          var table = datastore.getTable(type.tableName);
+          var values = $.map(table.query(), function (record) {
+            return $.extend(record.getFields(), {"id": record.getId()});
+          });
+          resolve(values);
+        });
       });
     }
   });
+  App.ApplicationAdapter = App.DropboxDataStoreAdapter;
+  // App.store = DS.Store.create({
+  //   adapter: App.DropboxDataStoreAdapter.create()
+  // });
+  App.Item = DS.Model.extend({
+    title: DS.attr()
+  })
+  App.Item.reopenClass({
+    tableName: "items" // FIXME: table name should be converted as plulalization of the model name
+  });
+
   // -> router.js
   App.Router.reopen({
     //location: 'history'
@@ -37,27 +72,12 @@
   });
   App.ItemsRoute = Ember.Route.extend({
     model: function() {
-      return App.datastore.then(function(datastore) {
-        return new Ember.RSVP.Promise(function (resolve, reject) {
-          var table = datastore.getTable('items');
-          var values = $.map(table.query(), function (record) {
-            return $.extend(record.getFields(), {"id": record.getId()});
-          });
-          resolve(values);
-        });
-      });
+      return this.get('store').findAll('item');
     }
   });
   App.ItemRoute = Ember.Route.extend({
     model: function(params) {
-      return App.datastore.then(function(datastore) {
-        return new Ember.RSVP.Promise(function (resolve, reject) {
-          var table = datastore.getTable('items');
-          var record = table.get(params.item_id);
-          var value = $.extend(record.getFields(), {"id": record.getId()});
-          resolve(value);
-        });
-      });
+      return this.get('store').find('item', params.item_id);
     },
     actions: {
       update: function(item) {
