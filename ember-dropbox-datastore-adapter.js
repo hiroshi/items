a318d4855208f2b1e3bd4e25df0f2b458921b5f7
Hiroshi Saito <hiroshi3110@gmail.com>
2013-12-08 10:52:01 +0900

    Separate ember-dropbox-datastore-adapter.js from item.js

---
+++ ember-dropbox-datastore-adapter.js
@@ -0,0 +1,145 @@
+// DropboxDataStoreAdapter
+// Usage example:
+//   App.ApplicationAdapter = DropboxDataStoreAdapter("your dropbox app key here", App);
+function DropboxDataStoreAdapter(key, App) {
+  // Dropbox Client
+  var client = new Dropbox.Client({key: key});
+  // Try to finish OAuth authorization.
+  client.authenticate({interactive: true}, function(error) {
+    if (error) {
+      alert('Authentication error: ' + error);
+    }
+  });
+  // Promise for Dropbox datastore
+  var datastore = new Ember.RSVP.Promise(function(resolve, reject) {
+    if (client.isAuthenticated()) {
+      var datastoreManager = client.getDatastoreManager();
+      datastoreManager.openDefaultDatastore(function(error, datastore) {
+        if (error) {
+          alert('Error opening default datastore: ' + error);
+          reject(error);
+        } else {
+          observeRmoteChanges(datastore);
+          resolve(datastore);
+        }
+      });
+    }
+  });
+  // Observe other end of change in the Dropbox DataStore
+  function observeRmoteChanges(datastore) {
+    datastore.recordsChanged.addListener(function(event) {
+      if (!event.isLocal()) {
+        var dbRecords = event.affectedRecordsByTable();
+        for (var dbTableName in dbRecords) {
+          dbRecords[dbTableName].forEach(function(dbRecord) {
+            var modelName = Ember.Inflector.inflector.singularize(dbTableName);
+            // http://stackoverflow.com/a/19406357/338986
+            var store = App.__container__.lookup('store:main');
+            store.find(modelName, dbRecord.getId()).then(function(record) {
+              if (record) {
+                if (dbRecord.isDeleted()) {
+                  // delete
+                  record.deleteRecord();
+                } else {
+                  // update
+                  record.setProperties(dbRecord.getFields());
+                }
+              } else {
+                // insert
+                // NOTE: find() just get the job done. It seems nothing to do here.
+              }
+            });
+          });
+        }
+      }
+    });
+  }
+  // convert a dropbox datastore ecord to a ember record
+  function emberRecord(model, dbRecord) {
+    var attrNames = Ember.get(model, 'attributes').keys.toArray();
+    var dbFields = dbRecord.getFields();
+    // Not a attribute, so keep it in noAttrs
+    var noAttrs = {};
+    for (var dbFieldName in dbFields) {
+      if (!attrNames.contains(dbFieldName)) {
+        noAttrs[dbFieldName] = dbFields[dbFieldName];
+      }
+    }
+    return $.extend(dbFields, {"id": dbRecord.getId(), "noAttrs": noAttrs});
+  }
+  // Dropbox DataStore API Ember.js Data Adapter
+  var adapter = DS.Adapter.extend({
+    find: function(store, type, id) {
+      return datastore.then(function(datastore) {
+        var dbTableName = Ember.Inflector.inflector.pluralize(type.typeKey);
+        var dbTable = datastore.getTable(dbTableName);
+        var dbRecord = dbTable.get(id);
+        var model = App.get(type.typeKey.capitalize());
+        value = emberRecord(model, dbRecord);
+        return Ember.RSVP.resolve(value);
+      }).fail(function(error) {
+         console.error(error);
+      });
+    },
+    findQuery: function(store, type, query, recordArray) {
+      return datastore.then(function(datastore) {
+        var dbTableName = Ember.Inflector.inflector.pluralize(type.typeKey);
+        var dbTable = datastore.getTable(dbTableName);
+        var model = App.get(type.typeKey.capitalize());
+        var dbQuery = typeof(model.dbQuery) == "function" ? model.dbQuery(query) : query;
+        var values = $.map(dbTable.query(dbQuery), function(dbRecord) {
+          return emberRecord(model, dbRecord);
+        });
+        return Ember.RSVP.resolve(values);
+      }).fail(function(error) {
+         console.error(error);
+      });
+    },
+    createRecord: function(store, type, record) {
+      return datastore.then(function(datastore) {
+        var dbTableName = Ember.Inflector.inflector.pluralize(type.typeKey);
+        var dbTable = datastore.getTable(dbTableName);
+        var value = record.toJSON();
+        var dbFields = $.extend(value, value['noAttrs']);
+        delete dbFields['noAttrs'];
+        var dbRecord = dbTable.insert(dbFields);
+        value['id'] = dbRecord.getId();
+        return Ember.RSVP.resolve(value);
+      }).fail(function(error) {
+         console.error(error);
+      });
+    },
+    updateRecord: function(store, type, record) {
+      return datastore.then(function(datastore) {
+        var dbTableName = Ember.Inflector.inflector.pluralize(type.typeKey);
+        var dbTable = datastore.getTable(dbTableName);
+        var dbRecord = dbTable.get(record.id);
+        var emberFields = record.toJSON();
+        dbFields = $.extend(emberFields, emberFields['noAttrs']);
+        delete dbFields['noAttrs'];
+        dbRecord.update(dbFields);
+        return Ember.RSVP.resolve();
+      }).fail(function(error) {
+         console.error(error);
+      });
+    },
+    deleteRecord: function(store, type, record) {
+      return datastore.then(function(datastore) {
+        var dbTableName = Ember.Inflector.inflector.pluralize(type.typeKey);
+        var dbTable = datastore.getTable(dbTableName);
+        var dbRecord = dbTable.get(record.id);
+        dbRecord.deleteRecord();
+        return Ember.RSVP.resolve();
+      }).fail(function(error) {
+         console.error(error);
+      });
+    }
+  });
+  return adapter;
+}
+// DropboxDataStoreAdapter.Model
+// Usage example:
+//   App.YourModel = DropboxDataStoreAdapter.Model.extend({...});
+DropboxDataStoreAdapter.Model = DS.Model.extend({
+  noAttrs: DS.attr(),
+});
--- items.js
+++ items.js
@@ -1,195 +1,79 @@
 (function() {
-  // Dropbox Client
-  var client = new Dropbox.Client({key: "qidrlfs46ow3v25"});
-  //   Try to finish OAuth authorization.
-  client.authenticate({interactive: true}, function(error) {
-    if (error) {
-      alert('Authentication error: ' + error);
+  // http://stackoverflow.com/a/7616484/338986
+  function hashCode(str) {
+    var hash = 0, i, char;
+    if (str.length == 0) return hash;
+    for (i = 0, l = str.length; i < l; i++) {
+        hash  = ((hash << 5) - hash) + str.charCodeAt(i);
+        hash |= 0; // Convert to 32bit integer
     }
-  });
+    console.log(hash);
+    return hash;
+  }
+  function labelKey(label) {
+    return "label_" + Number(hashCode(label)).toString(36);
+  }
   // Ember.js Application
   window.App = Ember.Application.create({
     //LOG_TRANSITIONS: true, 
     //LOG_TRANSITIONS_INTERNAL: true
   });
-  // promise for Dropbox datastore
-  var datastore = new Ember.RSVP.Promise(function(resolve, reject) {
-    if (client.isAuthenticated()) {
-      var datastoreManager = client.getDatastoreManager();
-      datastoreManager.openDefaultDatastore(function(error, datastore) {
-        if (error) {
-          alert('Error opening default datastore: ' + error);
-          reject(errot);
-        } else {
-          observeRmoteChanges(datastore);
-          resolve(datastore);
-        }
-      });
-    }
-  });
-  // observe remote changes
-  // FIXME: work with other tables than 'items'
-  function observeRmoteChanges(datastore) {
-    datastore.recordsChanged.addListener(function(event) {
-      if (!event.isLocal()) {
-        // FIXME: any tables
-        var dbRecords = event.affectedRecordsForTable('items');
-        dbRecords.forEach(function(dbRecord) {
-          App.Item.store.find('item', dbRecord.getId()).then(function(record) {
-            if (record) {
-              if (dbRecord.isDeleted()) {
-                record.deleteRecord();
-              } else {
-                record.set('title', dbRecord.get('title'));
-              }
-            } else {
-              // NOTE: find() just get the job done. nothing to do here?
-            }
-          });
-        });
-      }
-    });
-  }
-  // Dropbox DataStore API Ember.js Data Adapter
-  App.DropboxDataStoreAdapter = DS.Adapter.extend({
-    find: function(store, type, id) {
-      return datastore.then(function(datastore) {
-        return new Ember.RSVP.Promise(function(resolve, reject) {
-          var table = datastore.getTable(type.tableName);
-          var dbRecord = table.get(id);
-          var labels = Ember.A();
-          var value = $.extend(dbRecord.getFields(), {"id": dbRecord.getId(), "labels": labels});
-          resolve(value);
-        });
-      });
-    },
-    findAll: function(store, type, since) {
-      return datastore.then(function(datastore) {
-        return new Ember.RSVP.Promise(function(resolve, reject) {
-          var table = datastore.getTable(type.tableName);
-          var values = $.map(table.query(), function(dbRecord) {
-            var fields = dbRecord.getFields()
-            var labels = Ember.A();
-            for (var key in fields) {
-              if (key.match(/^label_(.*)$/)) {
-                labels.addObject(fields[key]);
-              }
-            }
-            return $.extend(dbRecord.getFields(), {"id": dbRecord.getId(), "labels": labels});
-          });
-          resolve(values);
-        });
-      });
-    },
-    findQuery: function(store, type, query, recordArray) {
-      return datastore.then(function(datastore) {
-        return new Ember.RSVP.Promise(function(resolve, reject) {
-        var dbQuery = {};
-        if (query['labels']) {
-          query['labels'].forEach(function(label) {
-            var key = "label_" + btoa(unescape(encodeURIComponent(label)));
-            dbQuery[key] = label;
-          });
-        }
-        //console.log(JSON.stringify(dbQuery));
-        var table = datastore.getTable(type.tableName);
-        var values = $.map(table.query(dbQuery), function(dbRecord) {
-          var fields = dbRecord.getFields()
-          var labels = Ember.A();
-          for (var key in fields) {
-            if (key.match(/^label_(.*)$/)) {
-              labels.addObject(fields[key]);
-            }
-          }
-          return $.extend(dbRecord.getFields(), {"id": dbRecord.getId(), "labels": labels});
-        });
-        //return Ember.RSVP.resolve(values);
-        resolve(values);
-      });
-      });
-    },
-    createRecord: function(store, type, record) {
-      return datastore.then(function(datastore) {
-        var table = datastore.getTable(type.tableName);
-        var value = record.toJSON();
-        delete value['labels']; // FIXME: store labels
-        var dbRecord = table.insert(value);
-        value = record.toJSON();
-        value['id'] = dbRecord.getId();
-        //record.set('id', dbRecord.getId());
-        return Ember.RSVP.resolve(value);
-      }).fail(function(error) {
-         console.error(error);
-      });
-    },
-    updateRecord: function(store, type, record) {
-      return datastore.then(function(datastore) {
-        var table = datastore.getTable(type.tableName);
-        var dbRecord = table.get(record.id);
-        dbRecord.set('title', record.get('title'));
-        var currentLabels = record.get('labels') || Ember.A();
-        for (key in dbRecord.getFields()) {
-          var match = key.match(/^label_(.*)$/);
-          if (match && !currentLabels.contains(dbRecord.get(key))) {
-            dbRecord.set(key, null);
-          }
-        }
-        // labels remains
-        currentLabels.forEach(function(label) {
-          var key = "label_" + btoa(unescape(encodeURIComponent(label)));
-          dbRecord.set(key, label);
-        });
-        return Ember.RSVP.resolve();
-      }).fail(function(error) {
-         console.error(error);
-      });
-    },
-    deleteRecord: function(store, type, record) {
-      return datastore.then(function(datastore) {
-        var table = datastore.getTable(type.tableName);
-        var dbRecord = table.get(record.id);
-        dbRecord.deleteRecord();
-        return Ember.RSVP.resolve();
-      });
-    }
-  });
-  App.ApplicationAdapter = App.DropboxDataStoreAdapter;
-  // App.store = DS.Store.create({
-  //   adapter: App.DropboxDataStoreAdapter.create()
-  // });
-  // App.Label = DS.Model.extend({
-  //   name: DS.attr()
-  // })
-  App.Item = DS.Model.extend({
+  // The dropbox datastore adapter
+  App.ApplicationAdapter = DropboxDataStoreAdapter("qidrlfs46ow3v25", App);
+  // Item
+  App.Item = DropboxDataStoreAdapter.Model.extend({
     title: DS.attr(),
     pos: DS.attr(),
     autoSave: function() {
       this.save();
-    }.observes('title'),
-    labels: DS.attr(),
-    // labelsChanged: function() {
-    //   console.log(this.get("labels"));
-    //   this.save();
-    // }.observes("labels.@each")
-
-    // labels: function(key, value) {
-    //   if (arguments.length > 1) {
-    //     this.set("_labels", value);
-    //   }
-    //   return this.get("_labels");
-    // }
-    // labels: function() {
-    //   return ["foo", "bar"];
-    // }.property()
-    //labels: DS.hasMany('label')
-    // get: function(name) {
-    //   console.log(name);
-    //   return "hello";
-    // }
-  })
-  App.Item.reopenClass({
-    tableName: "items", // FIXME: table name should be converted as plulalization of the model name
+    }.observes("title"),
+    // labels
+    labels: function() {
+      var labels = Ember.A();
+      var noAttrs = this.get('noAttrs');
+      for (var key in noAttrs) {
+        if (key.match(/^label_(.*)$/)) {
+          labels.addObject(noAttrs[key]);
+        }
+      }
+      return labels;
+    }.property("noAttrs.@each"),
+    labelsChanged: function() {
+      var labels = this.get('labels');
+      var noAttrs = this.get('noAttrs');
+      // First, clear label fields
+      for (var key in noAttrs) {
+        if (key.match(/^label_(.*)$/)) {
+          noAttrs[key] = null;
+        }
+      }
+      // Keep only current labels
+      labels.forEach(function(label) {
+        noAttrs[labelKey(label)] = label;
+      });
+      this.save();
+    }.observes("labels.@each")
   });
+  App.Item.reopenClass({
+    dbQuery: function(inQuery) {
+      var outQuery = {};
+      if (inQuery['labels']) {
+        inQuery['labels'].forEach(function(label) {
+          var key = "label_" + btoa(unescape(encodeURIComponent(label)));
+          outQuery[key] = label;
+        });
+      }
+      return outQuery;
+    }
+  });
+  // Laebel
+  // App.Label = DropboxDataStoreAdapter.Model.extend({
+  //   name: DS.attr(),
+  // });
+  // App.Label.reopenClass({
+  //   tableName: "labels", // FIXME: table name should be converted as plulalization of the model name
+  // });
+
 
   // -> router.js
   App.Router.reopen({
